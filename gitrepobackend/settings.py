1f770f722524c20df234338c753d816627ac9acd
sagar kumar <sagarkumarsameer@gmail.com>
2023-01-15 19:44:33 +0530

    database connected

--- gitrepobackend/settings.py
+++ gitrepobackend/settings.py
@@ -68,10 +68,10 @@ WSGI_APPLICATION = 'gitrepobackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
