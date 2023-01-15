d14b31132fe36c23288b49a35308c9ce1e986b04
sagar kumar <sagarkumarsameer@gmail.com>
2023-01-15 19:41:05 +0530

    versel conf

--- gitrepobackend/settings.py
+++ gitrepobackend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-dsxc#5+edq&mjf^l1@08ed5*x!%^5mg*rqu9mme#nk-%8_-2tu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -68,10 +68,10 @@ WSGI_APPLICATION = 'gitrepobackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -162,3 +162,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
